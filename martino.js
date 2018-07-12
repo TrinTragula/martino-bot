8ea545dfad0b754c59b7a08f21edcb3fbfa08281
TrinTragula (Daniele) <daniele.scarinci@protonmail.com>
2018-07-12 22:07:58 +0200

    Martino prende forma

--- martino.js
+++ martino.js
@@ -1,5 +1,6 @@
 require('dotenv').load();
 const TelegramBot = require('node-telegram-bot-api');
+const Util = require('./Util');
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
@@ -13,22 +14,43 @@ var commands = {
     },
     "help": (msg, command, argument) => {
         const chatId = msg.chat.id;
-        bot.sendMessage(chatId, JSON.stringify(commands));
+        bot.sendMessage(chatId, "Ecco i miei appunti:\n\n/start\nBella regà\n\n/help\nQuesto messaggio\n\n/porcozio [nome facoltativo]\nNon fatemi arrabbiare\n\n/citazione [ricerca facoltativa]\nSaggezza dispensata a caso\nRispondo anche inline! (non offenderti Martino pls)", {
+            "reply_markup": {
+                "keyboard": [Object.keys(commands).map(x => "/" + x)]
+            }
+        });
     },
     "porcozio": (msg, command, argument) => {
+        const chatId = msg.chat.id;
         if (argument && argument.length > 0) {
             const oggetto = argument.join(" ");
-            const chatId = msg.chat.id;
             var msg = `${oggetto}, non devi rompere il cazzo porco zio`;
             bot.sendMessage(chatId, msg);
         } else {
             var msg = `Ma quanto te pare de esse divertente porco zio`;
             bot.sendMessage(chatId, msg);
         }
+    },
+    "citazione": (msg, command, argument) => {
+        const chatId = msg.chat.id;
+        if (argument && argument.length > 0) {
+            let cit = Util.citazioneRandom(argument.join(" "));
+            bot.sendMessage(chatId, cit || "Di cosa stai parlando?");
+        } else {
+            bot.sendMessage(chatId, Util.citazioneRandom());
+        }
     }
 };
 
 bot.onText(/\/(.+)/, (msg, match) => {
+    const command = match[1];
+    let splitted = command.split(' ');
+    if (splitted && splitted.length > 0 && commands[splitted[0]]) {
+        const command = splitted.shift();
+        commands[command](msg, command, splitted);
+    }
+});
+bot.onText(/\/(.+)\@martinobetabot/, (msg, match) => {
     const command = match[1];
     let splitted = command.split(' ');
     if (splitted && splitted.length > 0 && commands[splitted[0]]) {
@@ -49,21 +71,58 @@ var keywords = {
         const chatId = msg.chat.id;
         bot.sendMessage(chatId, "Ma che cazzo vuoi " + msg.from.first_name);
     },
+    "matricola": (msg) => {
+        const chatId = msg.chat.id;
+        bot.sendPhoto(chatId, 'img/martino.jpg');
+    }
 }
 
 bot.on('message', (msg) => {
     var keys = Object.keys(keywords);
     for (key of keys) {
-        if (msg.text.toString().toLowerCase().includes(key)) {
+        if (msg && msg.text && msg.text[0] != "/" && msg.text.toString().toLowerCase().includes(key)) {
             keywords[key](msg);
         }
     }
 });
 
-/*
-    Handle logging
-*/
+bot.on('inline_query', (msg) => {
+    const id = msg.id;
+    const query = msg.query;
+    let result = [];
+    if (query && query != "") {
 
-bot.on('message', (msg) => {
-    console.log(JSON.stringify(msg));
+    } else {
+        for (let i = 0; i <= 3; i++) {
+            let cit = Util.citazioneRandom();
+            result.push(Util.createInlineTextResponse(cit));
+        }
+    }
+    bot.answerInlineQuery(id, result, {
+        cache_time: 0
+    });
+});
+
+bot.on('new_chat_members', (msg) => {
+    const chatId = msg.chat.id;
+    if (msg.new_chat_member && (msg.new_chat_member.username || msg.new_chat_member.first_name)) {
+        if (msg.new_chat_member.username == "martinobetabot") {
+            bot.sendMessage(chatId, "Bella regà");
+        } else {
+            let name = msg.new_chat_member.first_name;
+            bot.sendMessage(chatId, `Daje, ${name}, scappi?`);
+        }
+    }
+});
+
+bot.on('left_chat_member', (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, `Poraccio.`);
+});
+
+bot.on('new_chat_photo', (msg) => {
+    Util.setMartinoProfilePic(bot, msg);
+});
+bot.on('delete_chat_photo', (msg) => {
+    Util.setMartinoProfilePic(bot, msg);
 });
